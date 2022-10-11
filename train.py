7bfa98179c650539939dd7694d842a84ed239958
LC C <1791684372@qq.com>
2022-10-11 15:26:47 +0800

    change dataset path

--- train.py
+++ train.py
@@ -1,6 +1,3 @@
-import datetime
-import logging
-import lpips
 import numpy as np
 import torch
 import argparse
@@ -19,28 +16,45 @@ from dataset.load_data import *
 from tqdm import tqdm
 from utils.loss_util import *
 from utils.common import *
+import lpips
 from config.config import args
-import logging
-from PIL import Image
-from PIL import ImageFile
-import os
 
 
-def demo_test(args, TestImgLoader, model, save_path, device):
-    tbar = tqdm(TestImgLoader)
+def train_epoch(args, TrainImgLoader, model, model_fn, optimizer, epoch, iters, lr_scheduler):
+    """
+    Training Loop for each epoch
+    """
+    tbar = tqdm(TrainImgLoader)
+    total_loss = 0
+    lr = optimizer.state_dict()['param_groups'][0]['lr']
     for batch_idx, data in enumerate(tbar):
-        model.eval()
-        test_model_fn(args, data, model, save_path, device)
-        desc = 'Test demo'
+        loss = model_fn(args, data, model, iters)
+        # backward and update
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        iters += 1
+        total_loss += loss.item()
+        avg_train_loss = total_loss / (batch_idx + 1)
+        desc = 'Training  : Epoch %d, lr %.7f, Avg. Loss = %.5f' % (epoch, lr, avg_train_loss)
         tbar.set_description(desc)
         tbar.update()
+    lr = optimizer.state_dict()['param_groups'][0]['lr']
+    # the learning rate is adjusted after each epoch
+    lr_scheduler.step()
+
+    return lr, avg_train_loss, iters
 
 
 def init():
     # Make dirs
-    args.TEST_RESULT_DIR = os.path.join(args.SAVE_PREFIX, args.EXP_NAME, 'test_result')
-    mkdir(args.TEST_RESULT_DIR)
+    args.LOGS_DIR = os.path.join(args.SAVE_PREFIX, args.EXP_NAME, 'logs')
     args.NETS_DIR = os.path.join(args.SAVE_PREFIX, args.EXP_NAME, 'net_checkpoints')
+    args.VISUALS_DIR = os.path.join(args.SAVE_PREFIX, args.EXP_NAME, 'train_visual')
+    mkdir(args.LOGS_DIR)
+    mkdir(args.NETS_DIR)
+    mkdir(args.VISUALS_DIR)
+
     os.environ["CUDA_VISIBLE_DEVICES"] = "%d" % args.GPU_ID
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -56,146 +70,83 @@ def init():
         torch.backends.cudnn.deterministic = False
         torch.backends.cudnn.benchmark = True
 
-    return device
+    # summary writer
+    logger = SummaryWriter(args.LOGS_DIR)
+
+    return logger, device
 
 
-def load_checkpoint(model):
-    if args.LOAD_PATH:
-        load_path = args.LOAD_PATH
-        save_path = args.TEST_RESULT_DIR + '/customer'
-        log_path = args.TEST_RESULT_DIR + '/customer_result.log'
-    else:
-        print('Please specify a checkpoint path in the config file!!!')
-        raise NotImplementedError
-    mkdir(save_path)
-    if load_path.endswith('.pth'):
-        model_state_dict = torch.load(load_path)
-    else:
-        model_state_dict = torch.load(load_path)['state_dict']
+def load_checkpoint(model, optimizer, load_epoch):
+    load_dir = args.NETS_DIR + '/checkpoint' + '_' + '%06d' % load_epoch + '.tar'
+    print('Loading pre-trained checkpoint %s' % load_dir)
+    model_state_dict = torch.load(load_dir)['state_dict']
     model.load_state_dict(model_state_dict)
+    optimizer_dict = torch.load(load_dir)['optimizer']
+    optimizer.load_state_dict(optimizer_dict)
+    learning_rate = torch.load(load_dir)['learning_rate']
+    iters = torch.load(load_dir)['iters']
+    print('Learning rate recorded from the checkpoint: %s' % str(learning_rate))
 
-    return load_path, save_path, log_path
+    return learning_rate, iters
 
-
-def set_logging(log_path):
-    logger = logging.getLogger()
-    logger.setLevel(level=logging.DEBUG)
-
-    formatter = logging.Formatter('%(message)s')
-
-    file_handler = logging.FileHandler(log_path, mode='w')
-    file_handler.setLevel(level=logging.INFO)
-    file_handler.setFormatter(formatter)
-
-    stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(logging.WARNING)
-    stream_handler.setFormatter(formatter)
-
-    logger.addHandler(file_handler)
-    logger.addHandler(stream_handler)
-
-def test_model_fn(args, data, model, save_path, device):
-    # prepare input and forward
-    in_img = data['in_img'].to(device)
-    number = data['number']
-    b, c, h, w = in_img.size()
-
-    # pad image such that the resolution is a multiple of 32
-    w_pad = (math.ceil(w/32)*32 - w) // 2
-    h_pad = (math.ceil(h/32)*32 - h) // 2
-    in_img = img_pad(in_img, w_r=w_pad, h_r=h_pad)
-
-    with torch.no_grad():
-        out_1, out_2, out_3 = model(in_img)
-        if h_pad != 0:
-           out_1 = out_1[:, :, h_pad:-h_pad, :]
-        if w_pad != 0:
-           out_1 = out_1[:, :, :, w_pad:-w_pad]
-
-    # save images
-    if args.SAVE_IMG:
-        out_save = out_1.detach().cpu()
-        torchvision.utils.save_image(out_save, save_path + '/' + 'test_%s' % number[0] + '.%s' % args.SAVE_IMG)
-
-def create_demo_dataset(
-    args,
-    data_path,
-):
-    def _list_image_files_recursively(data_dir):
-        file_list = []
-        for home, dirs, files in os.walk(data_dir):
-            for filename in files:
-                ext = filename.split(".")[-1]
-                if ext.lower() in ["jpg", "jpeg", "png", "gif", "webp"] and filename[-5]=='e':
-                    file_list.append(os.path.join(home, filename))
-        file_list.sort()
-        return file_list
-
-
-    data_files = _list_image_files_recursively(data_dir=data_path)
-    dataset = demo_data_loader(data_files)
-
-    data_loader = data.DataLoader(
-        dataset, batch_size=args.BATCH_SIZE, shuffle=True, num_workers=args.WORKER, drop_last=True
-    )
-
-    return data_loader
-
-class demo_data_loader(data.Dataset):
-
-    def __init__(self, image_list):
-        self.image_list = image_list
-
-    def __getitem__(self, index):
-        ImageFile.LOAD_TRUNCATED_IMAGES = True
-        data = {}
-        path_src = self.image_list[index]
-        number = os.path.split(path_src)[-1]
-        number = number.split('.')[0]
-
-        img = Image.open(path_src).convert('RGB')
-        img = default_toTensor(img)
-
-        data['in_img'] = img
-        data['number'] = number
-
-        return data
-
-    def __len__(self):
-        return len(self.image_list)
 
 def main():
-    device = init()
-    # load model
+    logger, device = init()
+    # create model
     model = my_model(en_feature_num=args.EN_FEATURE_NUM,
                      en_inter_num=args.EN_INTER_NUM,
                      de_feature_num=args.DE_FEATURE_NUM,
                      de_inter_num=args.DE_INTER_NUM,
                      sam_number=args.SAM_NUMBER,
                      ).to(device)
+    model._initialize_weights()
 
-    # load checkpoint
-    load_path, save_path, log_path = load_checkpoint(model)
+    # create optimizer
+    optimizer = optim.Adam([{'params': model.parameters(), 'initial_lr': args.BASE_LR}], betas=(0.9, 0.999))
+    learning_rate = args.BASE_LR
+    iters = 0
+    # resume training
+    if args.LOAD_EPOCH:
+        learning_rate, iters = load_checkpoint(model, optimizer, args.LOAD_EPOCH)
+    # create loss function
+    loss_fn = multi_VGGPerceptualLoss(lam=args.LAM, lam_p=args.LAM_P).to(device)
+    # create learning rate scheduler
+    lr_scheduler = CosineAnnealingWarmRestarts(optimizer, T_0=args.T_0, T_mult=args.T_MULT, eta_min=args.ETA_MIN,
+                                               last_epoch=args.LOAD_EPOCH - 1)
+    # create training function
+    model_fn = model_fn_decorator(loss_fn=loss_fn, device=device)
+    # create dataset
+    train_path = args.TRAIN_DATASET
+    TrainImgLoader = create_dataset(args, data_path=train_path, mode='train')
 
-    # set logging for recording information or metrics
-    set_logging(log_path)
-    logging.warning(datetime.now())
+    # start training
+    print("****start traininig!!!****")
+    avg_train_loss = 0
+    for epoch in range(args.LOAD_EPOCH + 1, args.EPOCHS + 1):
+        learning_rate, avg_train_loss, iters = train_epoch(args, TrainImgLoader, model, model_fn, optimizer, epoch,
+                                                           iters, lr_scheduler)
+        logger.add_scalar('Train/avg_loss', avg_train_loss, epoch)
+        logger.add_scalar('Train/learning_rate', learning_rate, epoch)
 
-    logging.warning('load model from %s' % load_path)
-    logging.warning('save image results to %s' % save_path)
-    logging.warning('save logger to %s' % log_path)
+        # Save the network per ten epoch
+        if epoch % 10 == 0:
+            savefilename = args.NETS_DIR + '/checkpoint' + '_' + '%06d' % epoch + '.tar'
+            torch.save({
+                'learning_rate': learning_rate,
+                'iters': iters,
+                'optimizer': optimizer.state_dict(),
+                'state_dict': model.state_dict()
+            }, savefilename)
 
-    # Create dataset
-    test_path = args.DEMO_DATASET
-    # Set test batch size to 1 for avoiding OOM
-    args.BATCH_SIZE = 1
-    DemoImgLoader = create_demo_dataset(args, data_path=test_path)
-
-    # test demo
-    demo_test(args, DemoImgLoader, model, save_path, device)
+        # Save the latest model
+        savefilename = args.NETS_DIR + '/checkpoint' + '_' + 'latest.tar'
+        torch.save({
+            'learning_rate': learning_rate,
+            'iters': iters,
+            'optimizer': optimizer.state_dict(),
+            'state_dict': model.state_dict()
+        }, savefilename)
 
 
 if __name__ == '__main__':
     main()
-
-
